6467cd31e5a5e7d09a05c0966fb1194017f568da
kia1011 <a.kiaradibenedetto@icea.edu.ar>
2024-10-08 18:16:01 +0000

    h

--- kia.py
+++ kia.py
@@ -1,8 +1,19 @@
-.=kiara
-Npmbre= "valor"
-if(2+2)==4
-print/(nombre)
-while(nombre=valor)
-    print(hola)
-    nombre(hola)
+hola=1
+hola=True
+hola=False
+hola='hola'
+hola="hola"
+print(hola)
+for x in range (20):
+    print(x)
+    if 2+2==4:
+        print(true)
+        while False:
+            print(0)
+            valor='kiara'
+            kiara="."
+            nombre=''
+
+
     
+
